269aa732e263139a61bc94e05a61aabd8fd01878
hooss-only <hoossonly@gmail.com>
2024-01-27 17:53:33 +0900

    feat: record joining

--- src/observers/voiceChannelObserver.js
+++ src/observers/voiceChannelObserver.js
@@ -1,13 +1,30 @@
 export function observeVoiceChannel(client, dataManager) {
 	client.on("voiceStateUpdate", (_, newState) => {
 		if (newState.channel == null) {
-			console.log(dataManager.data);
-			console.log('bye');
+			// when leaves
 		} else {
-			// when join
-			dataManager.data['12-302-123'] = 10;
-			dataManager.save()
-			console.log('hi');
+			let date = dataManager.makeDataName();
+			let data = dataManager.data;
+			let userId = newState.id;
+			let hours = new Date().getHours() - 1;
+
+			dataManager.integrityVerification(date);
+			
+			if (hours != data.latestHours) {
+				data.voiceChannelTrace = [];
+			}
+
+			if (!data.voiceChannelTrace.includes(userId)) {
+				data.voiceChannelTrace.push(userId);
+				if (data[date].voice.length == 0)
+					data[date].voice = [0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+				data[date].voice[hours] += 1;
+			}
+			
+			data.latestHours = hours;
+			dataManager.data = data;
+
+			console.log(data);
 		}
 	});
 }
